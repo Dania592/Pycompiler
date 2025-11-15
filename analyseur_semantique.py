e35b5cbc54bf65f96bbd98c5a52b51fe0e4a03b4
doulkadi <dania.oulkadi@sfr.fr>
2025-11-15 14:04:34 +0100

    Suppression des commentaires inutiles dans analyseur semantique

--- analyseur_semantique.py
+++ analyseur_semantique.py
@@ -2,23 +2,22 @@ from analyseur_syntaxique import AnalyseurSyntaxique
 from Object import Node
 import config
 
-#Parcours l'arbre syntaxique pour effectuer des verifications afin de génerer le pseudo code machine
-class AnalyseurSemantique:    
+class AnalyseurSemantique:  
+    """
+    Parcours l'arbre syntaxique pour effectuer des verifications afin de génerer le pseudo code machine
+    """  
     def __init__(self, path ="text.txt", test = False, content = ""):
-        self.analyseur_syntaxique = AnalyseurSyntaxique(path, test, content) # recupere le resultat de l'analyseur syntaxique
+        self.analyseur_syntaxique = AnalyseurSyntaxique(path, test, content) 
         
-    # Méthode appelé pour générer le code machine
     def gencode(self):
         arbre = self.optim()
-        # print(arbre.afficher_arbre_joli())
-        # print("resn ", config.NB_VAR)
         config.CODE_ASM += "resn "+ str(config.NB_VAR) + "\n"
         self.gennode(arbre)
-        # print("drop ", config.NB_VAR)
+
         config.CODE_ASM += "drop "+ str(config.NB_VAR) + "\n"
-        
-    #fait l'analyse sémantique de l'arbre syntaxite avec 0 comme priorité initiale
+
     def optim(self):
+        """ fait l'analyse sémantique de l'arbre syntaxite avec 0 comme priorité initiale """
         arbre = self.anaSem()
         return arbre
     
@@ -27,48 +26,48 @@ class AnalyseurSemantique:
         config.NB_VAR = 0
         self.semNode(arbre)
         return arbre
-        
-    # Méthode qui parcours récursivement l'arbre de noeud et génère le code machine associé a chaque noeud
+
     def gennode(self, arbre : Node):
-        if (arbre.type ==  "node_const"):  #si le noeud est une constante on l'ajoute a la pile
-            # print("push ", arbre.valeur)
+        """ Méthode qui parcours récursivement l'arbre de noeud et génère le code machine associé a chaque noeud """
+        if (arbre.type ==  "node_const"):  
             config.CODE_ASM += "push "+ str(arbre.valeur) + "\n"
-        elif (arbre.type == "node_not"): # si le noeud correspond a un non logique
-            self.gennode(arbre.fils[0]) # on déroule la fonction gennode sur le fils 
+            
+        elif (arbre.type == "node_not"): 
+            self.gennode(arbre.fils[0]) 
             config.CODE_ASM += "not\n"
-            # print("not")
-        elif (arbre.type == "node_neg"): # si le neoud correspond à une négation arithmétique
+            
+        elif (arbre.type == "node_neg"): 
             config.CODE_ASM += "push 0\n"
-            # print("push 0")
             self.gennode(arbre.fils[0])
             config.CODE_ASM += "sub\n"
-            # print("sub")
+            
         elif (arbre.type == "node_debug"):
             self.gennode(arbre.fils[0])
             config.CODE_ASM += "dbg\n"
-            # print("dbg")
+            
         elif(arbre.type == "node_send"): 
             self.gennode(arbre.fils[0])
             config.CODE_ASM += "send\n"
+            
         elif(arbre.type == "node_recv"): 
             config.CODE_ASM += "recv\n"
+            
         elif (arbre.type == "node_ret"):
             self.gennode(arbre.fils[0])
             config.CODE_ASM += "ret\n"
-            # print("ret")
-        elif ( arbre.type in config.op_assembleur.keys() ): # si le type du noeud existe dans notre dictoinnaire 
+            
+        elif ( arbre.type in config.op_assembleur.keys() ):
             prefixe = config.op_assembleur[arbre.type]["prefixe"]
             if prefixe != "" :
                 config.CODE_ASM += prefixe + "\n"
-                # print(prefixe)
+                
             for fils in arbre.fils:
-                self.gennode(fils) # on fait de la recursivité sur les fils pour parcourir toute l'expression
+                self.gennode(fils) 
             suffixe = config.op_assembleur[arbre.type]["suffixe"]
             if suffixe != "":
                 config.CODE_ASM += suffixe + "\n"
-                # print(suffixe)
+                
         elif (arbre.type == "node_assign"):
-    
             if arbre.fils[0].type == "node_ind":
                 self.gennode(arbre.fils[1]) 
                 config.CODE_ASM += "dup\n"           
@@ -77,34 +76,28 @@ class AnalyseurSemantique:
             else: 
                 self.gennode(arbre.fils[1])
                 config.CODE_ASM += "dup\n"
-                # print("dup")
-                # print("set ", arbre.fils[0].index)
                 config.CODE_ASM += "set " + str(arbre.fils[0].index) + "\n"
         
         elif (arbre.type == "node_ref"):
             config.CODE_ASM += "get " +  str(arbre.index) + "\n"
-            # print("get ", arbre.index)
+            
         elif (arbre.type == "node_drop"):
             self.gennode(arbre.fils[0])
             config.CODE_ASM += "drop 1\n"
-            # print("drop 1")
             
         elif arbre.type == "node_cond":
             config.NB_LB += 1
             l = config.NB_LB
             self.gennode(arbre.fils[0])
             config.CODE_ASM += f"jumpf l{l}a\n"
-            # print(f"jumpf l{l}a")
-            self.gennode(arbre.fils[1]) ## EMELIE 
+            self.gennode(arbre.fils[1])
             config.CODE_ASM += f"jump l{l}b\n"  
-            # print(f"jump l{l}b")
             config.CODE_ASM += f".l{l}a\n"
-            # print(f".l{l}a")
+            
             if len(arbre.fils) > 2 and arbre.fils[2] is not None:
                 self.gennode(arbre.fils[2])
-
+                
             config.CODE_ASM += f".l{l}b\n"
-            # print(f".l{l}b")
             
             
         elif(arbre.type == "node_loop"): 
@@ -117,17 +110,17 @@ class AnalyseurSemantique:
                 config.CODE_ASM += f".l{config.ll}a\n"
                 
                 # Générer la condition
-                self.gennode(arbre.fils[0].fils[0])  # La condition
-                config.CODE_ASM += f"jumpf l{config.ll}b\n"  # Si faux, sortir
+                self.gennode(arbre.fils[0].fils[0])  
+                config.CODE_ASM += f"jumpf l{config.ll}b\n" 
                 
                 # Générer le corps de la boucle
                 if len(arbre.fils[0].fils) > 1:
-                    self.gennode(arbre.fils[0].fils[1])  # Le corps
+                    self.gennode(arbre.fils[0].fils[1])  
                 
-                config.CODE_ASM += f"jump l{config.ll}a\n"  # Retour au début
-                config.CODE_ASM += f".l{config.ll}b\n"  # Sortie
+                config.CODE_ASM += f"jump l{config.ll}a\n" 
+                config.CODE_ASM += f".l{config.ll}b\n" 
+                
             else:
-                # Boucle for ou autre type de boucle
                 config.CODE_ASM += f".l{config.ll}a\n"
                 
                 for i in range(len(arbre.fils)):
@@ -139,35 +132,29 @@ class AnalyseurSemantique:
             config.ll = temp
             
         elif(arbre.type == "node_break"): 
-            # print(f"jump l{config.ll}b")
             config.CODE_ASM += f"jump l{config.ll}b\n"
+            
         elif(arbre.type == "node_continue"):
-            # print(f"jump l{config.ll}c")
             config.CODE_ASM += f"jump l{config.ll}a\n"
+            
         elif(arbre.type == "node_target"): 
-            # print(f".l{config.ll}c")
             config.CODE_ASM += f".l{config.ll}c\n"
+            
         elif arbre.type == "node_seq":
             for fils in arbre.fils:
                 self.gennode(fils)
-
+                
         elif(arbre.type == "node_fonct"): 
-            # print(f".{arbre.chaine}")
             config.CODE_ASM += f".{arbre.chaine}\n"
-            # print(f"resn {arbre.nbArg}" ) # il faut compléter ici car on dois reserver le nombre de case pour les arguments
             config.CODE_ASM += f"resn {arbre.nbArg}\n"
-            self.gennode(arbre.fils[-1]) # on genere le code du dernier enfant uniquement
-            # print("push 0")
+            self.gennode(arbre.fils[-1])
             config.CODE_ASM += "push 0\n"
-            # print("ret")
             config.CODE_ASM += "ret\n"
-        
+            
         elif(arbre.type == "node_appel"): 
-            # print(f"prep {arbre.fils[0].chaine}") # on recupere le nom 
             config.CODE_ASM += f"prep {arbre.fils[0].chaine}\n"
-            for arg in arbre.fils[1:]: # on fait un gennode de tous les enfants excepté le 1er
+            for arg in arbre.fils[1:]: 
                 self.gennode(arg)
-            # print(f"call {len(arbre.fils) - 1}")   # nombre d'arguments
             config.CODE_ASM += f"call {len(arbre.fils) - 1}\n"
 
         elif(arbre.type == "node_ind"): 
@@ -177,9 +164,7 @@ class AnalyseurSemantique:
         elif arbre.type == "node_adr":
             var = arbre.fils[0]
             config.CODE_ASM += f"push {var.index}\n"
-
-        
-        
+            
             
     def begin(self):
         config.TS.append({})
@@ -187,11 +172,13 @@ class AnalyseurSemantique:
             config.var_stack = []
         config.var_stack.append(config.NB_VAR)
         
+        
     def end(self):
         table = config.TS.pop()
         nb_vars_liberated = len(table)
         config.NB_VAR = config.var_stack.pop()
         return nb_vars_liberated
+    
     
     def declare(self, name: str) -> dict :
         if not config.TS:
@@ -205,12 +192,12 @@ class AnalyseurSemantique:
         return sym
     
     
-    
     def find(self, name: str) -> dict:
-        for table in reversed(config.TS):  # on parcourt les scopes de haut en bas
-            if name in table:              # si la variable existe dans cette table
-                return table[name]         # on renvoie son symbole
+        for table in reversed(config.TS):  
+            if name in table:             
+                return table[name]    
         raise Exception(f"La variable '{name}' n'existe pas")
+    
 
     def semNode(self, arbre : Node):
         if (arbre.type ==  "node_block"):
@@ -219,11 +206,10 @@ class AnalyseurSemantique:
                 self.semNode(fils)
             nb_vars = self.end()
             arbre.nbArg = nb_vars
+            
         elif (arbre.type == "node_decl"):
             s = self.declare(arbre.chaine) 
             config.TS[-1][arbre.chaine]["index"] = config.NB_VAR
-            # print(config.TS)
-            
             config.NB_VAR += 1
         
         elif (arbre.type == "node_ref"):
@@ -243,7 +229,7 @@ class AnalyseurSemantique:
                 if fils.type == "node_decl":
                     config.NB_ARG += 1
             self.end()
-            arbre.nbArg = config.NB_ARG - (len(arbre.fils) - 1)
+            arbre.nbArg = len(arbre.fils) - 1
 
         elif arbre.type == "node_appel" : 
             for fils in arbre.fils:
